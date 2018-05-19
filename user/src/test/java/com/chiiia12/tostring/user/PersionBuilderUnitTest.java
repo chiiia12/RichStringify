6ea3e60fcc7a3b159583f6f8be611affaf176616
chiiia12 <llnsbchsep12@gmail.com>
2018-05-19 22:49:14 +0900

    fix bad service configuration

--- user/src/test/java/com/chiiia12/tostring/user/PersionBuilderUnitTest.java
+++ user/src/test/java/com/chiiia12/tostring/user/PersionBuilderUnitTest.java
@@ -2,12 +2,14 @@ package com.chiiia12.tostring.user;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class PersionBuilderUnitTest {
     @Test
     public void whenBuildPersonWithBuilder_thenObjectHasPropertyValues() {
-//        com.chiiia12.tostring.user.Person person = new PersonBuilder().setAge(25).setName("John").build();
-//////        assertEquals(25, person.getAge());
-//////        assertEquals("John", person.getName());
+        com.chiiia12.tostring.user.Person person = new PersonBuilder().setAge(25).setName("John").build();
+        assertEquals(25, person.getAge());
+        assertEquals("John", person.getName());
     }
 
 }

78a117de4d234f6313b67ba5a9ea0a2f1638acad
chiiia12 <llnsbchsep12@gmail.com>
2018-05-20 15:05:27 +0900

    add temporaly implementation

--- processor/src/main/java/com/chiiia12/tostring/processor/ToStringLabelProcessor.java
+++ processor/src/main/java/com/chiiia12/tostring/processor/ToStringLabelProcessor.java
@@ -2,7 +2,11 @@ package com.chiiia12.tostring.processor;
 
 import com.google.auto.service.AutoService;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.Processor;
@@ -10,15 +14,45 @@ import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.type.ExecutableType;
+import javax.tools.Diagnostic;
 
 @SupportedAnnotationTypes("com.chiiia12.tostring.processor.ToStringLabel")
 @SupportedSourceVersion(SourceVersion.RELEASE_8)
 @AutoService(Processor.class)
 public class ToStringLabelProcessor extends AbstractProcessor {
-   
+
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-        return false;
+        for (TypeElement annotation : annotations) {
+            Set<? extends Element> annotatedElements = roundEnv.getElementsAnnotatedWith(annotation);
+            Map<Boolean, List<Element>> annotatedField = annotatedElements.stream().collect(Collectors.partitioningBy(element ->
+                    ((ExecutableType) element.asType()).getParameterTypes().size() == 1 && element.getSimpleName().toString().startsWith("set")));
+            List<Element> setters = annotatedField.get(true);
+            List<Element> otherField = annotatedField.get(false);
+
+            otherField.forEach(element ->
+                    processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "@BuilderProperty must be applied a setXxx method with a single argument", element));
+            if (setters.isEmpty()) {
+                continue;
+            }
+            String className = ((TypeElement) setters.get(0).getEnclosingElement()).getQualifiedName().toString();
+            Map<String, String> setterMap = setters.stream().collect(Collectors.toMap(
+                    setter -> setter.getSimpleName().toString(),
+                    setter -> ((ExecutableType) setter.asType()).getParameterTypes().get(0).toString()
+            ));
+            try {
+                writeBuilderFile(className, setterMap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+        return true;
+    }
+
+    private void writeBuilderFile(String className, Map<String, String> setterMap) {
     }
 }

03d22f29badf07eb5c6c9433ad818b43a85b386d
chiiia12 <llnsbchsep12@gmail.com>
2018-06-07 21:33:05 +0900

    tweak a bit

--- user/src/test/java/com/chiiia12/tostring/user/PersionBuilderUnitTest.java
+++ user/src/test/java/com/chiiia12/tostring/user/PersionBuilderUnitTest.java
@@ -2,25 +2,15 @@ package com.chiiia12.tostring.user;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-
 public class PersionBuilderUnitTest {
     @Test
-    public void whenBuildPersonWithBuilder_thenObjectHasPropertyValues() {
-        PersonBuilder personBuilder = new PersonBuilder();
-        Person person = personBuilder.setAge(25).setName("John").build();
-        assertEquals(25, person.getAge());
-        assertEquals("John", person.getName());
-//        assertEquals("toString", personBuilder.toString());
-//        System.out.println("toString(): " + personBuilder.toString());
-    }
-
-    @Test
     public void whenBuildPersonWithStringify_thenObjectHasPropertyValues() {
+        //person test
         PersonBuilder personBuilder = new PersonBuilder();
         Person person = personBuilder.setAge(25).setName("John").build();
-        System.out.println(new Stringify(person).toString());
+        System.out.println("toString(): " + new Stringify(person).toString());
 
+        //animal test
         Animal animal = new Animal();
         animal.age = 3;
         animal.name = "hoge";

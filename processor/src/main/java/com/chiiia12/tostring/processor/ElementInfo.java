b813346e43a907a9e34e1b6c8aece298d176fb37
chiiia12 <llnsbchsep12@gmail.com>
2018-06-30 13:47:42 +0900

    add ElementInfo

---
+++ processor/src/main/java/com/chiiia12/tostring/processor/ElementInfo.java
@@ -0,0 +1,9 @@
+package com.chiiia12.tostring.processor;
+
+import javax.lang.model.element.Element;
+
+public class ElementInfo {
+    String variableName;
+    String label;
+    Element element;
+}
